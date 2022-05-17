152a7846881f355ab9cd1b31b49012ac8bdc0f71
Stefan Milutinovic <milutinke@milutinke-2.local>
2022-05-17 17:20:42 +0200

    Finished 11th chapter

---
+++ src/components/UI/Input.js
@@ -0,0 +1,16 @@
+import { forwardRef } from "react";
+
+import classes from "./Input.module.css";
+
+const Input = forwardRef((props, ref) => {
+  return (
+    <div className={classes.input}>
+      <label htmlFor={props.input.id} className={props.label}>
+        {props.label}
+      </label>
+      <input ref={ref} {...props.input} />
+    </div>
+  );
+});
+
+export default Input;
